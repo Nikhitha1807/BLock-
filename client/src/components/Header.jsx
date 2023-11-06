230c3fb7dbdd5a17b37f2c1939f53493f52b2ac3
hosenur <nur08439@gmail.com>
2023-11-06 15:12:21 +0530

    Remove revoke from header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,7 +32,6 @@ export default function Header({ account, contract }) {
                             )}
 
                         </li>
-                        <li><button onClick={() => document.getElementById('my_modal_2').showModal()}>Revoke Access</button></li>
                     </ul>
                 </div>
             </div>
